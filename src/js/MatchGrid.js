cd5b8f04963271cf43f5b1e7d0acbe72ab463ab3
Maksym <doomys1234@gmail.com>
2023-08-12 00:44:18 +0300

    small refactoring

--- src/js/MatchGrid.js
+++ src/js/MatchGrid.js
@@ -1,12 +1,16 @@
-import { text } from "body-parser";
-
 export default class MatchGrid {
   constructor({ amountOfTiles }) {
     this.amountOfTiles = amountOfTiles;
+    this.shuffledArray = this.shuffleArray(this.createDynamicArray());
+    this.firsElement = null;
+    this.secondElement = null;
+  }
+  startGame() {
+    this.createTileElements();
+    this.makeTilesFlipped();
   }
 
   createTileElements() {
-    console.log("start");
     const list = document.querySelector(".list");
     for (let i = 1; i <= this.amountOfTiles; i++) {
       const tile = document.createElement("li");
@@ -14,57 +18,71 @@ export default class MatchGrid {
       list.appendChild(tile);
     }
   }
-  startGame() {
-    this.createTileElements();
-    this.flipTiles();
-  }
-  flipTiles() {
+
+  makeTilesFlipped() {
     const listItems = document.querySelectorAll(".list .tile");
-    console.log(listItems);
     listItems.forEach((tile, index) => {
-      tile.addEventListener("click", () => {
-        const hasTileText =
-          tile.querySelector("p.tile-text.is-hidden-text") !== null;
-        tile.classList.add("flipped");
-        if (!hasTileText) {
-          this.flipText(tile);
-          return;
-        }
-        this.flipTileWithText(tile);
-      });
+      tile.addEventListener("click", () => this.handleTileClick(tile, index));
     });
   }
-  flipTileWithText(tile) {
+  handleTileClick(tile, index) {
     const textElement = tile.querySelector("p.tile-text.is-hidden-text");
-    setTimeout(() => {
-      textElement.classList.remove("is-hidden-text");
-    }, 500);
-    setTimeout(() => {
-      this.removeTextElement(tile);
-    }, 1500);
-    setTimeout(() => {
-      tile.classList.remove("flipped");
-    }, 2500);
+    const textValue = this.shuffledArray[index];
+    const doesTileContainText = textElement !== null;
+    tile.classList.add("flipped");
+
+    if (doesTileContainText) {
+      this.flipTileWithText(tile, textElement);
+    } else {
+      this.flipTileWithoutText(tile, textValue);
+    }
   }
-  flipText(tile, value) {
-    setTimeout(() => {
-      this.createTextElement(tile, 1);
-    }, 500);
-    setTimeout(() => {
-      this.removeTextElement(tile);
-    }, 1500);
-    setTimeout(() => {
-      tile.classList.remove("flipped");
-    }, 2500);
+  flipTileWithText(tile, textElement) {
+    setTimeout(() => this.showTextContent(textElement), 500);
+    setTimeout(() => this.hideTextContent(tile), 1500);
+    setTimeout(() => this.stopTileFlipping(tile), 2500);
   }
-  removeTextElement(tile) {
+  flipTileWithoutText(tile, textValue) {
+    setTimeout(() => this.createTextElement(tile, textValue), 500);
+    setTimeout(() => this.hideTextContent(tile), 1500);
+    setTimeout(() => this.stopTileFlipping(tile), 2500);
+  }
+  createTextElement(tile, textValue) {
+    const textElement = document.createElement("p");
+    textElement.classList.add("tile-text");
+    textElement.innerText = textValue;
+    tile.appendChild(textElement);
+  }
+  showTextContent(textElement) {
+    textElement.classList.remove("is-hidden-text");
+  }
+  hideTextContent(tile) {
     const textElement = tile.querySelector("p.tile-text");
     textElement.classList.add("is-hidden-text");
   }
-  createTextElement(tile, value) {
-    const textElement = document.createElement("p");
-    textElement.classList.add("tile-text");
-    textElement.innerText = value;
-    tile.appendChild(textElement);
+  stopTileFlipping(tile) {
+    tile.classList.remove("flipped");
+  }
+
+  createDynamicArray() {
+    const length = this.amountOfTiles;
+    const halfLength = length / 2;
+    const dynamicArray = [];
+
+    for (let i = 1; i <= length; i++) {
+      dynamicArray.push(((i - 1) % halfLength) + 1);
+    }
+    return dynamicArray;
+  }
+  shuffleArray(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
   }
 }
